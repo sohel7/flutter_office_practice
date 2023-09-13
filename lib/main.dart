cf176fee03aa6159106e4b52ea2c0191e37ee684
sohel7 <sohelocp@gmail.com>
2023-09-13 17:14:06 +0600

    commit first

--- lib/main.dart
+++ lib/main.dart
@@ -7,30 +7,97 @@ main(){
 
 class MyApp extends StatelessWidget{
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.green),
-      darkTheme: ThemeData(primarySwatch: Colors.amber ),
-      color: Colors.green,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      darkTheme: ThemeData(primarySwatch: Colors.blue ),
+      color: Colors.blue,
       debugShowCheckedModeBanner: false,
       home: HomeActivity(),
     );
   }
 }
 
+//  SHow Snackbar Message  IN AppBar
+//==================================
 class HomeActivity extends StatelessWidget{
   const HomeActivity({super.key});
+
+  MySnackBar(message,context){
+
+    ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(message))
+    );
+  }
 
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home"),),
-      body: Text("Hello WOrld"),
+
+      appBar: AppBar(
+        title: Text("Inventory"),
+        titleSpacing: 10,
+        toolbarHeight: 60,
+        toolbarOpacity: 1,
+        //elevation: 0,
+        elevation: 10,
+        backgroundColor: Colors.blue,
+
+        // ICON SET IN AppBar
+        actions: [
+          IconButton(onPressed: (){MySnackBar("message",context);}, icon: Icon(Icons.message)),
+          IconButton(onPressed: (){MySnackBar("Search",context);}, icon: Icon(Icons.search)),
+          IconButton(onPressed: (){MySnackBar("Settings", context);}, icon: Icon(Icons.settings)),
+        ],
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        elevation: 6,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.blue,
+        onPressed: (){
+          MySnackBar("I am FLoating Action Buttion",context);
+
+        },
+      ),
     );
   }
-
 }
+
+
+
+
+
+
+
+// // ICON SET IN AppBar
+// //=======================
+// class HomeActivity extends StatelessWidget{
+//   const HomeActivity({super.key});
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//
+//       appBar: AppBar(
+//         title: Text("Inventory"),
+//         titleSpacing: 10,
+//         toolbarHeight: 60,
+//         toolbarOpacity: 1,
+//         //elevation: 0,
+//         elevation: 10,
+//         backgroundColor: Colors.blue,
+//
+//         // ICON SET IN AppBar
+//         actions: [
+//           IconButton(onPressed: (){}, icon: Icon(Icons.message)),
+//           IconButton(onPressed: (){}, icon: Icon(Icons.search)),
+//           IconButton(onPressed: (){}, icon: Icon(Icons.settings)),
+//         ],
+//       ),
+//
+//     );
+//   }
+// }
