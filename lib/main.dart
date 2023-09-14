22bbdd86ec25fb26b5bef2f8ad0b34affa99664d
sohel7 <sohelocp@gmail.com>
2023-09-14 17:07:41 +0600

    commit first

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget{
 //  SHow Snackbar Message  IN AppBar
 //==================================
 class HomeActivity extends StatelessWidget{
-  const HomeActivity({super.key});
+   const HomeActivity({super.key});
 
   MySnackBar(message,context){
 
@@ -35,6 +35,18 @@ class HomeActivity extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
+
+    // ADD CUSTOM BUTTON STYLE for eleveted button
+    final ButtonStyle buttonStyle=ElevatedButton.styleFrom(
+        padding: EdgeInsets.all(20),
+        backgroundColor: Colors.green,
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(40))
+        )
+
+    );
+
     return Scaffold(
 
       appBar: AppBar(
@@ -53,124 +65,143 @@ class HomeActivity extends StatelessWidget{
           IconButton(onPressed: (){MySnackBar("Settings", context);}, icon: Icon(Icons.settings)),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        elevation: 6,
-        child: Icon(Icons.add),
-        backgroundColor: Colors.blue,
-        onPressed: (){
-          MySnackBar("I am FLoating Action Buttion",context);
-
-        },
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 2,// will show the active index
-        items: [
-
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Setting"),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Searct"),
+      // floatingActionButton: FloatingActionButton(
+      //   elevation: 6,
+      //   child: Icon(Icons.add),
+      //   backgroundColor: Colors.blue,
+      //   onPressed: (){
+      //     MySnackBar("I am FLoating Action Buttion",context);
+      //
+      //   },
+      // ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   currentIndex: 2,// will show the active index
+      //   items: [
+      //
+      //     BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
+      //     BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Setting"),
+      //     BottomNavigationBarItem(icon: Icon(Icons.search), label: "Searct"),
+      //   ],
+      // ),
+      // drawer: Drawer(
+      //   child: ListView(
+      //     children: [
+      //       // DrawerHeader(child:  Text("Sohel")),
+      //       DrawerHeader(
+      //         padding: EdgeInsets.all(0),
+      //         child: UserAccountsDrawerHeader(
+      //           decoration: BoxDecoration(color: Colors.white),
+      //           accountName: Text("Md SOhel Hossain", style: TextStyle(color: Colors.black),),
+      //           accountEmail: Text("sohelju1983@gmail.com",style: TextStyle(color: Colors.black),),
+      //           currentAccountPicture: Image.network("https://banner2.cleanpng.com/20180710/xui/kisspng-dart-programming-language-flutter-object-oriented-flutter-logo-5b454ed3adae62.4180922415312688197114.jpg"),
+      //           onDetailsPressed: (){
+      //             MySnackBar("Iam Details", context);
+      //           },
+      //
+      //
+      //         ),
+      //       ),
+      //       ListTile(
+      //           leading: Icon(Icons.home),
+      //           title: Text("home"),
+      //           onTap: (){
+      //             MySnackBar("Home",context);
+      //           }),
+      //       ListTile(
+      //           leading: Icon(Icons.contacts),
+      //           title: Text("contact"),
+      //           onTap: (){
+      //             MySnackBar("Contact",context);
+      //           }),
+      //       ListTile(
+      //           leading: Icon(Icons.person),
+      //           title: Text("profile"),
+      //           onTap: (){
+      //             MySnackBar("profile",context);
+      //           }),
+      //       ListTile(
+      //           leading: Icon(Icons.phone),
+      //           title: Text("phone"),
+      //           onTap: (){
+      //             MySnackBar("phone",context);
+      //           }),
+      //     ],
+      //   ),
+      // ),
+      // endDrawer: Drawer(
+      //   child: ListView(
+      //     children: [
+      //       // DrawerHeader(child:  Text("Sohel")),
+      //       DrawerHeader(
+      //         padding: EdgeInsets.all(0),
+      //         child: UserAccountsDrawerHeader(
+      //           decoration: BoxDecoration(color: Colors.white),
+      //           accountName: Text("Md SOhel Hossain", style: TextStyle(color: Colors.black),),
+      //           accountEmail: Text("sohelju1983@gmail.com",style: TextStyle(color: Colors.black),),
+      //           currentAccountPicture: Image.network("https://banner2.cleanpng.com/20180710/xui/kisspng-dart-programming-language-flutter-object-oriented-flutter-logo-5b454ed3adae62.4180922415312688197114.jpg"),
+      //           onDetailsPressed: (){
+      //             MySnackBar("Iam Details", context);
+      //           },
+      //
+      //
+      //         ),
+      //       ),
+      //       ListTile(
+      //           leading: Icon(Icons.home),
+      //           title: Text("home"),
+      //           onTap: (){
+      //             MySnackBar("Home",context);
+      //           }),
+      //       ListTile(
+      //           leading: Icon(Icons.contacts),
+      //           title: Text("contact"),
+      //           onTap: (){
+      //             MySnackBar("Contact",context);
+      //           }),
+      //       ListTile(
+      //           leading: Icon(Icons.person),
+      //           title: Text("profile"),
+      //           onTap: (){
+      //             MySnackBar("profile",context);
+      //           }),
+      //       ListTile(
+      //           leading: Icon(Icons.phone),
+      //           title: Text("phone"),
+      //           onTap: (){
+      //             MySnackBar("phone",context);
+      //           }),
+      //     ],
+      //   ),
+      // ),
+      // CONTAINE IN BODY
+      // body: Container(
+      //   height: 250,
+      //   width: 250,
+      //    alignment: Alignment.topCenter,
+      //   //margin: EdgeInsets.all(30),
+      //   margin: EdgeInsets.fromLTRB(60, 30, 20, 30),
+      //   //padding: EdgeInsets.all(30),
+      //   padding: EdgeInsets.fromLTRB(30, 30, 40, 50),
+      //   decoration: BoxDecoration(
+      //     color: Colors.blue,
+      //     border: Border.all(color: Colors.black,width: 6)
+      //   ),
+      //   child: Text("Hello",style: TextStyle(color: Colors.white),),
+      // ),
+      //
+    // BUTTON CREATE
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          TextButton(onPressed: (){MySnackBar("I am Text TextButton",context);}, child: Text('Text Button')),
+          ElevatedButton(onPressed: (){MySnackBar("I am Text ElevatedButton",context);}, child: Text('Text Button'),style: buttonStyle,),
+          OutlinedButton(onPressed: (){MySnackBar("I am Text OutlinedButton",context);}, child: Text('Text Button'))
         ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            // DrawerHeader(child:  Text("Sohel")),
-            DrawerHeader(
-              padding: EdgeInsets.all(0),
-              child: UserAccountsDrawerHeader(
-                decoration: BoxDecoration(color: Colors.white),
-                accountName: Text("Md SOhel Hossain", style: TextStyle(color: Colors.black),),
-                accountEmail: Text("sohelju1983@gmail.com",style: TextStyle(color: Colors.black),),
-                currentAccountPicture: Image.network("https://banner2.cleanpng.com/20180710/xui/kisspng-dart-programming-language-flutter-object-oriented-flutter-logo-5b454ed3adae62.4180922415312688197114.jpg"),
-                onDetailsPressed: (){
-                  MySnackBar("Iam Details", context);
-                },
-
-
-              ),
-            ),
-            ListTile(
-                leading: Icon(Icons.home),
-                title: Text("home"),
-                onTap: (){
-                  MySnackBar("Home",context);
-                }),
-            ListTile(
-                leading: Icon(Icons.contacts),
-                title: Text("contact"),
-                onTap: (){
-                  MySnackBar("Contact",context);
-                }),
-            ListTile(
-                leading: Icon(Icons.person),
-                title: Text("profile"),
-                onTap: (){
-                  MySnackBar("profile",context);
-                }),
-            ListTile(
-                leading: Icon(Icons.phone),
-                title: Text("phone"),
-                onTap: (){
-                  MySnackBar("phone",context);
-                }),
-          ],
-        ),
-      ),
-      endDrawer: Drawer(
-        child: ListView(
-          children: [
-            // DrawerHeader(child:  Text("Sohel")),
-            DrawerHeader(
-              padding: EdgeInsets.all(0),
-              child: UserAccountsDrawerHeader(
-                decoration: BoxDecoration(color: Colors.white),
-                accountName: Text("Md SOhel Hossain", style: TextStyle(color: Colors.black),),
-                accountEmail: Text("sohelju1983@gmail.com",style: TextStyle(color: Colors.black),),
-                currentAccountPicture: Image.network("https://banner2.cleanpng.com/20180710/xui/kisspng-dart-programming-language-flutter-object-oriented-flutter-logo-5b454ed3adae62.4180922415312688197114.jpg"),
-                onDetailsPressed: (){
-                  MySnackBar("Iam Details", context);
-                },
-
-
-              ),
-            ),
-            ListTile(
-                leading: Icon(Icons.home),
-                title: Text("home"),
-                onTap: (){
-                  MySnackBar("Home",context);
-                }),
-            ListTile(
-                leading: Icon(Icons.contacts),
-                title: Text("contact"),
-                onTap: (){
-                  MySnackBar("Contact",context);
-                }),
-            ListTile(
-                leading: Icon(Icons.person),
-                title: Text("profile"),
-                onTap: (){
-                  MySnackBar("profile",context);
-                }),
-            ListTile(
-                leading: Icon(Icons.phone),
-                title: Text("phone"),
-                onTap: (){
-                  MySnackBar("phone",context);
-                }),
-          ],
-        ),
-      ),
+      ) ,
 
     );
   }
 }
-
-
-
-
-
-
 
 // // ICON SET IN AppBar
 // //=======================
