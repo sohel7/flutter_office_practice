2a4389a0913862d5f1e81a37bc643b4e1f9d2460
sohel7 <sohelocp@gmail.com>
2023-09-15 14:42:14 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,21 @@ class HomeActivity extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Home"),),
-      body: Text("Hello WOrld"),
+      
+     // FLOATING ACTION BAR
+      //==================================
+        floatingActionButton: FloatingActionButton(
+        elevation: 6,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.blue,
+        onPressed: (){
+          MySnackBar("I am FLoating Action Buttion",context);
+
+        },
+      ),
+
+      
+      
     );
   }
 
